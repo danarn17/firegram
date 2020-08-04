1ae5147fa18d59f6c1e0e2408dc5f0d40b9afcaa
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-04 15:59:07 +0700

    Updates

---
+++ precache-manifest.24ceeff5c3e9ee533d1fb2c8524d51b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3014f386c557eb7b3df57552d183ef",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "8755acddb60b22d3d644",
+    "url": "/firegram/static/css/main.2567098d.chunk.css"
+  },
+  {
+    "revision": "47009bb72e3a16741577",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8755acddb60b22d3d644",
+    "url": "/firegram/static/js/main.c96cc887.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
